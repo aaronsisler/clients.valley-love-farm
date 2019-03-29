14af0ac30ce2d24e5ef35d50808ba9acaac7d5a2
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-03-29 11:48:28 -0400

    Update: Removing farm house pic and updating GoogleMap coordinates in config

--- src/content/gallery.js
+++ src/content/gallery.js
@@ -1,52 +1,48 @@
 const gallery = [
     {
-        imagePath: 'images/gallery1.jpg',
-        alt: 'Gallery 1',
+        imagePath: "images/gallery1.jpg",
+        alt: "Gallery 1"
     },
     {
-        imagePath: 'images/gallery2.jpg',
-        alt: 'Gallery 2',
+        imagePath: "images/gallery2.jpg",
+        alt: "Gallery 2"
     },
     {
-        imagePath: 'images/gallery3.jpg',
-        alt: 'Gallery 3',
+        imagePath: "images/gallery3.jpg",
+        alt: "Gallery 3"
     },
     {
-        imagePath: 'images/gallery4.jpg',
-        alt: 'Gallery 4',
+        imagePath: "images/gallery5.jpg",
+        alt: "Gallery 5"
     },
     {
-        imagePath: 'images/gallery5.jpg',
-        alt: 'Gallery 5',
+        imagePath: "images/gallery6.jpg",
+        alt: "Gallery 6"
     },
     {
-        imagePath: 'images/gallery6.jpg',
-        alt: 'Gallery 6',
+        imagePath: "images/gallery7.jpg",
+        alt: "Gallery 7"
     },
     {
-        imagePath: 'images/gallery7.jpg',
-        alt: 'Gallery 7',
+        imagePath: "images/gallery8.jpg",
+        alt: "Gallery 8"
     },
     {
-        imagePath: 'images/gallery8.jpg',
-        alt: 'Gallery 8',
+        imagePath: "images/gallery9.jpg",
+        alt: "Gallery 9"
     },
     {
-        imagePath: 'images/gallery9.jpg',
-        alt: 'Gallery 9',
+        imagePath: "images/gallery10.jpg",
+        alt: "Gallery 10"
     },
     {
-        imagePath: 'images/gallery10.jpg',
-        alt: 'Gallery 10',
+        imagePath: "images/gallery11.jpg",
+        alt: "Gallery 11"
     },
     {
-        imagePath: 'images/gallery11.jpg',
-        alt: 'Gallery 11',
-    },
-    {
-        imagePath: 'images/gallery12.jpg',
-        alt: 'Gallery 12',
+        imagePath: "images/gallery12.jpg",
+        alt: "Gallery 12"
     }
-]
+];
 
 export default gallery;
