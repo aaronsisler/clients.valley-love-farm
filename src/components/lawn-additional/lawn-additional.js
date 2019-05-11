baedaac81e45772f0fab1c665aec12f89f176067
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-05-11 11:50:29 -0400

    Sliding in the lawn components

---
+++ src/components/lawn-additional/lawn-additional.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./lawn-additional.scss";
+
+const LawnAdditional = () => (
+  <div className="lawn-additional">LawnAddtional</div>
+);
+
+export default LawnAdditional;
---
+++ src/components/lawn/lawn.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./lawn.scss";
+
+const Lawn = () => <div className="lawn">Lawn</div>;
+
+export default Lawn;
