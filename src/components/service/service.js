01d27d8ea610680f6ea67ad700603f2ad194145a
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-05-12 20:30:33 -0400

    Adding in the splitting of services into sub component and content. Working through CSS port now

---
+++ src/components/service/service.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./service.scss";
+
+const Service = ({ image, linkPath, text, title }) => (
+  <div className="service">
+    <div className="service__image">
+      <img src={image.source} alt={image.altText} />
+    </div>
+    <div className="service__content">
+      <div className="service__content-title">{title}</div>
+      <div className="service__content-text">{text}</div>
+      <Link to={`/${linkPath}`} className="link">
+        Find Out More
+      </Link>
+    </div>
+  </div>
+);
+
+Service.propTypes = {
+  image: PropTypes.shape({
+    altText: PropTypes.string.isRequired,
+    source: PropTypes.string.isRequired
+  }),
+  linkPath: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+export default Service;
--- src/components/services/services.js
+++ src/components/services/services.js
@@ -1,6 +1,17 @@
 import React from "react";
+import services from "../../content/services";
+import Service from "../../components/service";
 import "./services.scss";
 
-const Services = () => <div className="services">Services</div>;
+const Services = () => (
+  <div className="services">
+    <h3>What We Provide</h3>
+    <div className="services__content">
+      {services.map(service => (
+        <Service key={service.key} {...service} />
+      ))}
+    </div>
+  </div>
+);
 
 export default Services;
