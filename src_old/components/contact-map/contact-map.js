5bf85b2e6a05af0651efacc50799c935c2b148a6
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-06-22 20:41:59 -0400

    Removing old source code from CRA style project

--- src_old/components/contact-map/contact-map.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./contact-map.scss";
-
-const ContactMap = () => <div className="contact-map">ContactMap</div>;
-
-export default ContactMap;
--- src_old/config/config.js
+++
@@ -1,9 +0,0 @@
-//Analytics
-export const analyticsTrackingId = "UA-115632487-1";
-
-// Email
-export const pointOfContactEmail = "valleylovehs@gmail.com";
-
-//Services Gateway Config
-export const servicesApiGatewayURL =
-  "https://2des5sl17b.execute-api.us-east-1.amazonaws.com/prod/send-email";
