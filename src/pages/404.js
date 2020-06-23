6d100a37fd4b179d175f8569dbbb18da875a4a35
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-06-22 21:09:34 -0400

    Adding in 404 page and switching hamburger placement

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ErrorContainer from "../containers/error-container";
+
+const Custom404Page = () => {
+  return (
+    <main>
+      <ErrorContainer />
+    </main>
+  );
+};
+
+export default Custom404Page;
