b76291885dbbb43ed3503ee759090f1745ef249b
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2026-08-24 20:48:12 -0400

    Updating all the things

--- src/next.config.js
+++ src/next.config.js
@@ -1,14 +1,8 @@
 module.exports = {
+  output: "export",
   distDir: "../build",
   trailingSlash: true,
   images: {
-    loader: "imgix",
-    path: "/",
-    domains: [
-      "cdn-e-and-b-solutions.s3.amazonaws.com",
-      "cdn-e-and-b-solutions.s3.amazonaws.com/clients/valley-love-farm",
-      "beta.valleylovefarm.com",
-      "valleylovefarm.com",
-    ],
+    unoptimized: true,
   },
 };
